87bf5b71167a9732fdf141fdd615f921fe2909e3
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-06-09 14:38:38 +0700

    fix: change condition logic

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ func main() {
 	scores := []int{50, 75, 66, 20, 32, 90}
 	var newScores [7]int
 	for x := range scores {
-		if scores[x] == 20 {
-			newScores[x] = 88
-			newScores[x+1] = scores[x]
-		} else if x > 3 {
+		if scores[x] == 66 {
+			newScores[x] = scores[x]
+			newScores[x+1] = 88
+		} else if x > 2 {
 			newScores[x+1] = scores[x]
 		} else {
 			newScores[x] = scores[x]
